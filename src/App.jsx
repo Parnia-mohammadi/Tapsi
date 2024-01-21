1c0d47fb0565c0ad624de10531c80dead07a78d3
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-01-21 16:59:28 +0330

    install tailwind and create by vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css"
+function App() {
+  return (
+    <div class="text-blue-600 bg-red-300">
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <div>
+        
+        <ul>
+            <li>ویدیو ها</li>
+            <li>پشتیبانی</li>
+            <li>امور سهام </li>
+            <li>تپسی پک</li>
+            <li>فرصت های شغلی</li>
+            <li>وبلاگ</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Header
