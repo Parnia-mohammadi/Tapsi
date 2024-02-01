36922e6066250bcf4fc8191862887da6a46edaf8
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-02-01 18:48:24 +0330

    responsive service tab was built

--- src/components/Header/Home.jsx
+++ src/components/Header/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Banner from "../Banner/Banner";
 import Header from "./Header";
+import TapsiService from "../tapsi-service/TapsiService";
 
 function Home() {
   // for changing heroicons and showing drop down navbar
@@ -9,9 +10,11 @@ function Home() {
     setIsNavOpen((is) => !is);
   };
   return (
-    <div >
-      <Header isNavOpen={isNavOpen} handleNavbar={handleNavbar}/>
+    <div>
+      <Header isNavOpen={isNavOpen} handleNavbar={handleNavbar} class="sticky top-0"/>
       <Banner isNavOpen={isNavOpen}/>
+      <TapsiService/>
+      <div>hellllllooo</div>
     </div>
   );
 }
